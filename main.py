b8b5276ebcf190868616e9204e64c18297f0516e
Rania <rania@Ranias-Mac-mini.local>
2025-12-08 14:08:43 -0500

    FastAPI and UV Setup

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
-def main():
-    print("Hello from notetakinghelper!")
+# from fastapi import FastAPI
+
+# app = FastAPI()
+
+# @app.get("/")
+# def root():
+#     return {"message": "Hello World"}
+
+# def main():
+#     print("Hello from notetakinghelper!")
 
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
+
+
+# main.py
+from typing import Union
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
